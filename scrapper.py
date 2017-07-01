80256e24ab89d8e36b4c5d07635abfe908e33b74
Palmirouze <Palmirouze@gmail.com>
2017-07-01 14:26:27 -0400

    added excel output

--- scrapper.py
+++ scrapper.py
@@ -1,40 +1,27 @@
+# DEPENDENCIES
+# - ChromeDriver
+# - Selenium
+# - BeautifulSoup
+# - Python 3.x
+
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
+import csv
 
-#You need to install ChromeDriver and use Python 3.xx with Selenium and BeautifulSoup
+# USER CREDENTIALS
+USER_NAME = "X_XXXXX"
+USER_PASSWORD = "XXXXXXXXXX"
+CHROME_DRIVER_PATH = "XXXXXXXXXX"
 
-# ------  put your own credentials here ------
-userNetname = "xxxx"
-userPassword = "xxxx"
 
-
-gradeList = []
-driver = webdriver.Chrome(executable_path="/Users/Palmirouze/Desktop/python_grade_scrapper/chromedriver")
-driver.get("https://my.concordia.ca")
-
-netnameBox = driver.find_element_by_xpath('//*[@id="userid"]')
-passwordBox = driver.find_element_by_id("pwd")
-
-netnameBox.send_keys(userNetname)
-passwordBox.send_keys(userPassword)
-
-driver.find_element_by_class_name("form_button_submit").click()
-time.sleep(1)
-driver.find_element_by_id("fldra_CU_MY_STUD_CENTRE").click()
-time.sleep(1)
-driver.find_element_by_partial_link_text("View My Grades").click()
-time.sleep(1)
-
-driver.switch_to.frame("ptifrmtgtframe")
-
-#selects a grade from the radio buttons available
-#top of the list is semesterNumber = 0, second element is 1 etc.
-#number of courses is the number of course per specific semester
+#gets all the grades for a specific semester and number of courses taken this semester
 def getGradesForSemester(semesterNumber, numberOfCourses):
     driver.find_element_by_id("SSR_DUMMY_RECV1$sels$"+str(semesterNumber)+"$$0").click()
     driver.find_element_by_id("DERIVED_SSS_SCT_SSR_PB_GO").click()
     driver.implicitly_wait(3)
+
+    #scrapes all the grades
     scrapeGrades(numberOfCourses)
 
     #goes back to semester selection page
@@ -43,7 +30,7 @@ def getGradesForSemester(semesterNumber, numberOfCourses):
 
 #gets the data from one semester page
 def scrapeGrades(numberOfCourses):
-    time.sleep(1)
+    time.sleep(SLEEP_TIME)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     driver.implicitly_wait(3)
 
@@ -55,31 +42,91 @@ def scrapeGrades(numberOfCourses):
         gradeList.append(soup.find(id="DERIVED_SSS_HST_DESCRSHORT$" + str(i)).get_text())
         i = i+1
 
-    time.sleep(1)
+    time.sleep(SLEEP_TIME)
     driver.find_element_by_id("ICTAB_1_52").click()
-    time.sleep(1)
+    time.sleep(SLEEP_TIME)
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     driver.implicitly_wait(3)
 
     #get course grade distribution
+    count = 0
     for distribution in soup.find_all("span", class_="PSEDITBOX_DISPONLY"):
-        gradeList.append(distribution.get_text())
+
+        if(count == 0):
+            tempGradeList = []
+
+        tempGradeList.append(distribution.get_text())
+        count = count + 1
+
+        if(count == 17):
+            distributionList.append(tempGradeList)
+            count = 0
 
 
+def mergeLists():
+    i = 0
+    for elements in distributionList:
+        elements.insert(0, gradeList[i])
+        i = i + 1
+        elements.insert(2, gradeList[i])
+        i = i + 1
 
-#first parameter is position of semester you want in the list in View My Grades
+def outputToCSV():
+    with open("output.csv", "w") as f:
+        writer = csv.writer(f)
+
+        writer.writerow(gradeLegend)
+
+        for lists in distributionList:
+            writer.writerow(lists)
+
+
+#MAIN
+
+#Increase sleep time if connexion is slow
+SLEEP_TIME = 0.3
+
+gradeList = []
+distributionList = []
+gradeLegend = ["COURSE NUMBER", "COURSE NAME", "GRADE", "A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+",
+               "D", "D-", "F", "FNS", "R", "NR"]
+
+driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+driver.get("https://my.concordia.ca")
+
+# login
+netnameBox = driver.find_element_by_xpath('//*[@id="userid"]')
+passwordBox = driver.find_element_by_id("pwd")
+netnameBox.send_keys(USER_NAME)
+passwordBox.send_keys(USER_PASSWORD)
+
+# navigate to grade screen
+driver.find_element_by_class_name("form_button_submit").click()
+time.sleep(SLEEP_TIME)
+driver.find_element_by_id("fldra_CU_MY_STUD_CENTRE").click()
+time.sleep(SLEEP_TIME)
+driver.find_element_by_partial_link_text("View My Grades").click()
+time.sleep(SLEEP_TIME)
+driver.switch_to.frame("ptifrmtgtframe")
+
+#first parameter is position of semester you want in the list in View My Grades (Starts at 0)
 #second is the number of courses you took during this semester
-getGradesForSemester(3, 3)
+#example:
+getGradesForSemester(0, 4) # winter 2018 - 4 courses
+getGradesForSemester(1, 5) # fall 2017 - 5 courses
+getGradesForSemester(2, 2) # summer 2017 - 2 courses
+getGradesForSemester(3, 3) # winter 2017 - 3 courses
 getGradesForSemester(4, 4)
 getGradesForSemester(5, 4)
 getGradesForSemester(6, 4)
 getGradesForSemester(7, 2)
 getGradesForSemester(8, 4)
 
-#print all the data
-for grades in gradeList:
+
+mergeLists()
+
+for grades in distributionList:
     print(grades)
 
-#this data can be plugged in excel to plot
-#will eventually add excel export
+outputToCSV()
